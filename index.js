6fee0b118da4ede77829d2e775edf2a6bd89c535
César Gtz <cesareomargtz@gmail.com>
2018-04-12 19:46:40 -0600

    El frontend y Electron

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict'
+
+// Instanciando los Objetos app y BrowserWindopw
+const { app, BrowserWindow } = require('electron')
+
+// Realiza un dir de todo el contenido del objeto app
+// console.dir(app)
+
+//Imprimiendo un Mensaje en la consola ante de salir
+app.on('before-quit', () => {
+  console.log('Saliendo..')
+})
+
+//Ejecutando ordenes cuando la aplicacion este lista.
+app.on('ready', () => {
+  //creando una ventana. Para agregar propiedades pasarlas por medio de un objeto
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: 'Hola Mundo',
+    center: true,
+    maximizable: false,
+    show: false
+  })
+
+  /*evento en on() se ejecuta multiple veces y once() una sola vez.
+  rady-to-show carga la vista de una url
+  agregar la propiedad show: false*/
+  win.once('ready-to-show',() => {
+    win.show()
+  })
+
+  //cargar contenido desde remoto
+  //win.loadURL('http://devdocs.io/')
+  //${__dirname} es un helper que devolvera el directorio actual de donde estoy
+  win.loadURL(`file://${__dirname}/renderer/index.html`)
+
+  //detectando el cierre de la ventana para cerra el aplicativo
+  win.on('close', () => {
+    win = null //limpiar el objeto
+    app.quit()
+  })
+
+  //cuando la ventana se mueve imprime sus cordenadas
+  win.on('move', () => {
+    const position = win.getPosition()
+    console.log(`la variable position es ${position}`)
+  })
+
+})
---
+++ renderer/frontend.js
@@ -0,0 +1,4 @@
+//cada vez que se carge va a insertar el mensaje
+window.addEventListener('load', () => {
+  document.getElementById('message').innerHTML = 'Esto es un mensaje insertado desde JS'
+})
